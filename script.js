fb64efb83b6034fb90443c8047d9172203d03df1
Daniel Rovira <danielroviratorres@gmail.com>
2024-07-02 19:10:08 -0300

    mudei a forma que remove as divs, apenas ocultando

--- script.js
+++ script.js
@@ -1,15 +1,50 @@
-const form = document.querySelector('form')
+const filterButton = document.getElementById("filterButton")
 
-form.addEventListener('submit', async (event) => {
+let list = [
+    "drenagem",
+    "abastecimento",
+    "SANEAMENTO",
+    "ELEVATÓRIA",
+    "TRATAMENTO",
+    "esgot",
+    "ADUTOR"
+]
+
+filterButton.addEventListener('click', async (event) => {
+    function get_elements_by_inner(words) {
+        let res = []
+        const elems = [...document.getElementsByClassName('sc-caiLqq eUEOWK col-md-11')];
+    
+        words.forEach((word) => {
+            let normalizeWord = word.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase()
+            elems.forEach((elem) => { 
+                let selected = elem.firstChild.innerHTML.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase()
+                elem.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.style.display = 'none'
+                if(selected.includes(normalizeWord)) {
+                    res.push(elem)
+                }
+            })
+        })
+
+        res.forEach((re) => {
+            elems.forEach((elem) => {
+                if (re === elem) {
+                    elem.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.style.display = 'flex'
+                }
+            })
+        })
+    }
+
     event.preventDefault();
 
     const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
 
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        files : [ "filtro.js" ],
+        // files : [ "filter.js" ],
+        func: get_elements_by_inner,
+        args: [list]
 
-        // function: get_elements_by_inner(list),
     });
 
 });
